00d7af9a826ed8bb825424fa8e8065b02d552cd9
Muusy <112031065+FFUV@users.noreply.github.com>
2024-01-17 21:11:01 -0500

    Update intelli_bot.py

--- intelli_bot.py
+++ intelli_bot.py
@@ -1,58 +1,57 @@
 import websocket
 import ssl
+import requests
 from json import loads, dumps
-from commands.fun_commands import process_fun_command
-from commands.meme_commands import process_meme_command
-from utils.image_processing import process_imgur_link
-from utils.joke_fetcher import fetch_joke
+from PIL import Image
+from io import BytesIO
+import re
+import random
+from commands import process_imgur_link, fetch_joke, process_commands
+from utils import is_bot_mentioned
 
-# Configuration
-nick = 'IntelliBot'
-channel_to_join = 'programming'
+nick = 'intelliBOT'  # Change this to your bot name!!!
+channel_to_join = 'programming'  # Use lowercase 'programming'
 password = '<your password>'
-joke_api_url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=political,sexist'
+joke_api_url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=political,sexist'  # JokeAPI URL
 
-ws = websocket.create_connection("wss://hack.chat/chat-ws", sslopt={"cert_reqs": ssl.CERT_NONE})
-ws.send(dumps({'cmd': 'join', 'channel': channel_to_join, 'nick': nick, 'trip': 'datura', 'pass': password}))
+ws = websocket.create_connection("wss://hack.chat/chat-ws", sslopt={"cert_reqs": ssl.CERT_NONE})  # 1: connect
+ws.send(dumps({'cmd': 'join', 'channel': channel_to_join, 'nick': nick, 'trip': 'datura', 'pass': password}))  # 2: join
 print(f'The {nick} bot is now running...')
 
 while True:
-    try:
-        result = loads(ws.recv())
-        cmd = result['cmd']
+    result = loads(ws.recv())  # 3: receive data
+    print(str(result))
+    cmd = result['cmd']
 
-        if cmd == 'chat':
-            msg = result['text']
-            
-            # Process different commands based on cmd value
-            if "No Homo" in msg and "then homo" in msg.lower():
-                random_number = random.randint(1, 100)
-                response_msg = f"{random_number} homo"
-                ws.send(dumps({'cmd': 'chat', 'text': response_msg}))
+    if cmd == 'chat':  # If someone sent a message...
+        if result['nick'] == nick:
+            continue
 
-            # Check for Imgur links
-            if 'imgur.com' in msg:
-                process_imgur_link(msg)
+        msg = result['text']
 
-            # Check for !joke command
-            if msg.lower() == '!joke':
-                joke = fetch_joke()
-                ws.send(dumps({'cmd': 'chat', 'text': joke}))
+        # Check if intelliBOT is mentioned
+        if is_bot_mentioned(msg, nick):
+            response_msg = "Need help? Here are some commands:\n" + process_commands()
+            ws.send(dumps({'cmd': 'chat', 'text': response_msg}))
+            continue
 
-            # Process fun commands
-            process_fun_command(msg)
+        # Check for Imgur links
+        if 'imgur.com' in msg:
+            process_imgur_link(ws, msg)
 
-            # Process meme commands
-            process_meme_command(msg)
+        # Check for !joke command
+        if msg.lower() == '!joke':
+            joke = fetch_joke()
+            ws.send(dumps({'cmd': 'chat', 'text': joke}))
 
-        elif cmd == 'onlineSet':
-            print(f'Users online: {result["nicks"]}\nPowered by {nick}!')
+        # Process other commands
+        process_commands(ws, msg, nick, channel_to_join)
 
-        elif cmd == 'info':
-            if result['nick'] == nick and result['channel'] == channel_to_join:
-                ws.send(dumps({'cmd': 'op', 'channel': channel_to_join, 'nick': nick}))
-                introduction_msg = "Hello fellow humanoids. Type !help for commands."
-                ws.send(dumps({'cmd': 'chat', 'text': introduction_msg}))
+    elif cmd == 'onlineSet':
+        print(f'Users online: {result["nicks"]}\nPowered by {nick}!')
 
-    except Exception as e:
-        print(f"Error: {e}")
+    elif cmd == 'info':
+        if result['nick'] == nick and result['channel'] == channel_to_join:
+            ws.send(dumps({'cmd': 'op', 'channel': channel_to_join, 'nick': nick}))  # Attempt to become a mod
+            introduction_msg = "Hello fellow humanoids."
+            ws.send(dumps({'cmd': 'chat', 'text': introduction_msg}))
